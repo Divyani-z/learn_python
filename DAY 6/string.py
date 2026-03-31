86d90c09a0edbf8ba99b205f60b7065e095e9911
Divyani-z <divyanizade2005@gmail.com>
2026-03-31 23:21:28 +0530

    Update string

--- DAY 6/string.py
+++ DAY 6/string.py
@@ -1,2 +1,44 @@
 #String concatenation and conditional statements
 
+str1= "This is a string.\n This is a new line" 
+print(str1)
+
+str2=" Hello Riya\t How are you?" 
+print(str2)
+
+#Basic String operations  operations
+'''1. String Concatenation'''
+
+# "Hello" + "World"= "HelloWorld"     
+
+str1="Hello"
+str2="Riya"
+final_str= str1 +str2
+print(final_str)                
+
+#  '''2.String length'''
+
+print(len(name_str)) 
+name_str="Riya"
+
+yourName_str= input("Enter your name:")
+len1= len(yourName_str)
+print(len1)
+
+# '''3.Indexing '''
+Name= "Sanjana"
+print(Name[0])   
+print(Name[6])
+
+'''4. Slicing'''
+Role = "Python Developer"
+print(Role[1:8])
+
+Company= "Mirosoft Corporation"
+print(Company [:7])
+print(Company [0:]) 
+print(Company[-5:-1])
+
+
+
+

80557ade7973910ece91d56b77a572e17e286c4b
Divyani-z <divyanizade2005@gmail.com>
2026-04-04 23:52:17 +0530

    List

--- DAY 7/List.py
+++ DAY 7/List.py
@@ -3,24 +3,24 @@
 # it is separated by comma (,)
 
 '''List Slicing'''
-# a=[1,2,3,4,5]
-# print([4])
+a=[1,2,3,4,5]
+print([4])
 
 '''b=[1,2,3,4] #integer list
 c=["Hello","Bye"] #string list
 d=[1,2,[3,4,5,6]] #nested list
 '''
 
-# num=[1,2,3,4,5]
-# print(num[0: :2])
+num=[1,2,3,4,5]
+print(num[0: :2])
 
-# str=["hello","i","am","divyani"]
-# print(str[0: :1])
+str=["hello","i","am","divyani"]
+print(str[0: :1])
 
 '''TO REVERSE THE LIST'''
 
-# num=[1,2,3,4,5,6]
-# print(num[-1: :-1])
+num=[1,2,3,4,5,6]
+print(num[-1: :-1])
 
 '''LIST ITERATION'''
 # to find length formula: len(l)
